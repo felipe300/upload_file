3c0abdc55709845dc4f50ff9b70ad4e23b313b8b
Felipe300 <caracoloro@gmail.com>
2023-07-04 10:30:13 -0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Subida de archivos
+
+## Instalación
+
+```bash
+npm i
+```
+
+### Librerías utilizadas
+
+Librerías sequelize, pg, pg-hstore, express, express-fileupload, cors, morgan y dotenv.
+
+Librerías de desarrollo nodemon.
+
+## Arrancar el proyecto
+
+```bash
+npm run dev
+```
---
+++ src/config/db.config.js
@@ -0,0 +1,14 @@
+import Sequelize from 'sequelize'
+import 'dotenv/config'
+
+const sequelize = new Sequelize(
+	process.env.POSTGRES_DB,
+	process.env.POSTGRES_USER,
+	process.env.POSTGRES_PASS,
+	{
+		host: process.env.POSTGRES_HOST,
+		dialect: "postgres",
+	}
+)
+
+export default sequelize

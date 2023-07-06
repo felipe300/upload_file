615ae4029cb76ae288a7bde6c9d8df7ec1b5a68f
Felipe300 <caracoloro@gmail.com>
2023-07-06 09:26:27 -0400

    feat: add render db, do not allow upload files

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,27 @@
 import Sequelize from 'sequelize'
-import 'dotenv/config'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+import * as dotenv from 'dotenv'
+import pg from 'pg'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+let dialectOptions = null
+
+if (process.env.NODE_ENV === 'development') {
+	dotenv.config({
+		path: path.join(__dirname, '../../.env')
+	})
+} else {
+	dotenv.config({
+		path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`)
+	})
+	dialectOptions = {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false
+		}
+	}
+}
 
 const sequelize = new Sequelize(
 	process.env.POSTGRES_DB,
@@ -7,7 +29,16 @@ const sequelize = new Sequelize(
 	process.env.POSTGRES_PASS,
 	{
 		host: process.env.POSTGRES_HOST,
-		dialect: 'postgres'
+		port: process.env.DB_PORT || 5432,
+		dialect: 'postgres',
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 20000,
+			idle: 5000
+		},
+		dialectOptions,
+		dialectModule: pg
 	}
 )
 
